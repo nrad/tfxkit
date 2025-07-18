75e6792f2a5451c7c957c7105044f2900d229ce4
nrad <navidrad@Mac.fritz.box>
2025-07-18 10:54:05 +0200

    dynamically forward attr from model_factory to its components

--- src/tfxkit/core/model_factory.py
+++ src/tfxkit/core/model_factory.py
@@ -14,16 +14,58 @@ class ModelFactory:
         self.config_loader = ConfigLoader()
         self.config = self.config_loader.config
         self.data = DataManager(self.config)
-        self.model_builder = ModelBuilder(self.config)
-        self.model = self.model_builder
-        # self.model = self.model_builder.build
-        self.trainer = Trainer(self.config, self.model, self.data)
-        self.evaluator = Evaluator(self.config, self.model, self.data)
+        self.builder = ModelBuilder(self.config)
+        # self.buil = self.model_builder
+
+        self.trainer = Trainer(self.config, self.builder.model, self.data)
+        self.evaluator = Evaluator(self.config, self.builder.model, self.data)
+
+    def __getattr__(self, name):
+        """
+        Dynamically forwards attribute access to internal components (e.g., Trainer, Evaluator).
+
+        If exactly one component defines the requested attribute, it is returned.
+        If multiple components define the same attribute, raises an AttributeError to avoid ambiguity.
+        """
+        components = [self.trainer, self.evaluator]
+        matches = [getattr(c, name) for c in components if hasattr(c, name)]
+
+        if len(matches) > 1:
+            raise AttributeError(
+                f"Ambiguous attribute '{name}' found in multiple components: {', '.join(c.__class__.__name__ for c in components)}"
+            )
+
+        elif matches:
+            logger.debug(
+                f"Accessing attribute '{name}' from {matches[0].__class__.__name__}"
+            )
+            return matches[0]
+
+        raise AttributeError(
+            f"'{self.__class__.__name__}' object has no attribute '{name}'"
+        )
+    @forward("trainer")
+    def model(*args, **kwargs):
+
+        """Delegates to the Trainer's model method."""
+        pass
+
 
 if __name__ == "__main__":
-    setup_logging(level=logging.DEBUG)
     logger.info("ModelFactory starting up...")
     mf = ModelFactory()
+    setup_logging(level=mf.config.logging.level)
     mf.config_loader.print_config()
-    # mf.data_manager.load_df()
-    # mf.data_manager.df_trainm
+
+    # define and compile the model
+    # mf.model = mf.builder.define_model()
+    mf.builder.compile()
+    logger.info("Model Created successfully.")
+
+    mf.fit(epochs=2)
+    logger.info("Model training completed.")
+
+    mf.evaluator.add_test_train_preds()
+    logger.info("Predictions added to train and test datasets.")
+
+    #
