eca1c2eb1d683a0887ef459a4f5bac86799e11c8
nrad <navid.khandan.rad@desy.de>
2025-09-05 17:29:33 +0200

    include generic tunning

--- src/tfxkit/core/tuner.py
+++ src/tfxkit/core/tuner.py
@@ -8,13 +8,31 @@ from pydoc import locate
 logger = logging.getLogger(__name__)
 
 
-class Tuner:
+class HyperTuner:
+    """
+    Controller class for executing a sequence of hyperparameter tuning steps using Keras Tuner.
+
+    This class iterates over a sequence of tuning steps defined in the configuration
+    and applies each one by dynamically importing and calling the specified function.
+    Each function builds a model and runs a tuner with custom settings.
+
+    Attributes:
+        config (OmegaConf): The full configuration object.
+        builder (ModelBuilder): Responsible for compiling models.
+        data (Namespace): Holds X_train, y_train, and optionally sample_weight_train.
+
+    Methods:
+        run_sequence():
+            Executes all tuner functions defined in the config's sequence.
+    """
     def __init__(self, config, builder, data):
         self.config = config
         self.builder = builder
         self.data = data
+        self.results = []
 
     def run_sequence(self):
+        self.tuners = []
         for name in self.config.tuner.sequence:
             func_config = self.config.tuner.functions[name]
             func_path = func_config.function
@@ -24,131 +42,271 @@ class Tuner:
             if func is None:
                 raise ImportError(f"Could not locate function '{func_path}'")
 
-            kwargs = OmegaConf.to_container(
-                func_config.get("parameters", {}), resolve=True
+            kwargs = OmegaConf.to_container(func_config.parameters, resolve=True)
+            settings = (
+                OmegaConf.to_container(func_config.get("settings"), resolve=True)
+                if func_config.get("settings")
+                else {}
             )
-            settings = OmegaConf.to_container(
-                func_config.get("settings", {}), resolve=True
-            )
+
             logger.info(f"Running tuner step: {name}")
             # func(self, **kwargs, **settings)
-            func(config=self.config, builder=self.builder, data=self.data, **kwargs, **settings)
+            model_builder = func(
+                config=self.config,
+                builder=self.builder,
+                data=self.data,
+                **kwargs,
+                **settings,
+            )
+            # self.results.append(result)
+            tuner = kt.BayesianOptimization(
+                model_builder,
+                objective="val_loss",
+                directory=name,
+                project_name=f"HPTuning",
+                max_trials=10,
+                num_initial_points=None,
+                alpha=0.0001,
+                beta=2.6,
+                seed=None,
+                overwrite=True,
+            )
+            tuner.search(
+                self.data.X_train,
+                self.data.y_train,
+                validation_split=0.2,
+                sample_weight=self.data.sample_weight_train,
+                batch_size=50_000,
+                # verbose=0,
+            )
+            self.tuners.append((name, tuner))
 
         logger.info("Tuning sequence completed.")
 
-def progressive_layer_tuning(
-    config, builder, data, 
-    units_choices=[], max_layers=5, patience=1, max_trials=20
-):
-    # cfg = self.config.tuner.progressive_layer
-    # max_layers = cfg.max_layers
-    # patience = cfg.patience
-    # max_trials = cfg.max_trials
-    # new_units_choices = cfg.units_choices
+    def generic_tuner(
+        config,
+        builder,
+        data,
+        **kwargs,
+    ):
+        """A generic tuner that can tune any hyperparameter specified in kwargs.
+        kwargs should be a dictionary where keys are the hyperparameter names,
+        corresponding to config fields and values are lists of possible values.
 
-    best_layers = []
-    best_val_loss = np.inf
-    no_improve_counter = 0
-    tuners = {}
-
-    for n_layers in range(1, max_layers + 1):
+        """
 
         def build_model(hp):
-            layers = best_layers.copy()
-            new_units = hp.Choice(f"units_layers_{n_layers}", units_choices)
-            layers.append(new_units)
-            model_kwargs = dict(layers_list=layers)
-            model = builder.define_model(**model_kwargs)
-            return builder.compile(model)
+            new_conf = config.copy()
+            for kwarg, choices in kwargs.items():
+                value = hp.Choice(kwarg, choices)
+                # OmegaConf.update(new_conf, f"model.parameters.{kwarg}", value)
+                OmegaConf.update(new_conf, kwarg, value)
+                print(f"Setting {kwarg} to {value}")
+                # config = new_conf
+            print(new_conf)
+            builder = ModelBuilder(new_conf)
+            return builder.compile()
 
-        tuner = kt.BayesianOptimization(
-            build_model,
-            objective="val_loss",
-            max_trials=max_trials,
-            directory="progressive_tune",
-            project_name=f"layers_{n_layers}",
-        )
-        tuners[n_layers] = tuner
-
-        tuner.search(
-            data.X_train,
-            data.y_train,
-            validation_split=0.2,
-            sample_weight=data.sample_weight_train,
-            verbose=0,
-        )
-
-        best_hp = tuner.get_best_hyperparameters(1)[0]
-        val_loss = tuner.oracle.get_best_trials(1)[0].score
-
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            best_layers.append(best_hp.get(f"units_layers_{n_layers}"))
-            no_improve_counter = 0
-        else:
-            no_improve_counter += 1
-            if no_improve_counter >= patience:
-                break
-
-    return best_layers, tuners
+        return build_model
 
 
+from tfxkit.core.model_builder import ModelBuilder
 
-# class LayerTuner(Tuner):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
 
-#     def progressive_layer_tuning(
-#         self, units_choices=[], max_layers=5, patience=1, max_trials=20
-#     ):
-#         # cfg = self.config.tuner.progressive_layer
-#         # max_layers = cfg.max_layers
-#         # patience = cfg.patience
-#         # max_trials = cfg.max_trials
-#         # new_units_choices = cfg.units_choices
+def optimizer_tuning(
+    config,
+    builder,
+    data,
+    optimizers=["keras.optimizers.adam", "keras.optimizers.sgd", "rmsprop"],
+    # learning_rates=[1e-2, 1e-3, 1e-4],
+):
+    """Tune the optimizer and learning rate."""
 
-#         best_layers = []
-#         best_val_loss = np.inf
-#         no_improve_counter = 0
-#         tuners = {}
+    def build_model(hp):
+        optimizer = hp.Choice("optimizer", optimizers)
+        new_conf = config.copy()
+        OmegaConf.update(new_conf, "optimizer.function", "keras.optimizers.Adam")
+        builder = ModelBuilder(new_conf)
+        return builder.compile()
 
-#         for n_layers in range(1, max_layers + 1):
+    return build_model
 
-#             def build_model(hp):
-#                 layers = best_layers.copy()
-#                 new_units = hp.Choice(f"units_layers_{n_layers}", units_choices)
-#                 layers.append(new_units)
-#                 model_kwargs = dict(layers_list=layers)
-#                 model = self.builder.define_model(**model_kwargs)
-#                 return self.builder.compile(model)
 
-#             tuner = kt.BayesianOptimization(
-#                 build_model,
-#                 objective="val_loss",
-#                 max_trials=max_trials,
-#                 directory="progressive_tune",
-#                 project_name=f"layers_{n_layers}",
-#             )
-#             tuners[n_layers] = tuner
+# def progressive_layer_tuning(
+#     config, builder, data,
+#     units_choices=[], max_layers=5, patience=1, max_trials=20
+# ):
+#     """ Progressively tune the number of layers and units in each layer.
+#     """
 
-#             tuner.search(
-#                 self.data.X_train,
-#                 self.data.y_train,
-#                 validation_split=0.2,
-#                 sample_weight=self.data.sample_weight_train,
-#                 verbose=0,
-#             )
+#     best_layers = []
+#     best_val_loss = np.inf
+#     no_improve_counter = 0
+#     tuners = {}
+#     builders = []
 
-#             best_hp = tuner.get_best_hyperparameters(1)[0]
-#             val_loss = tuner.oracle.get_best_trials(1)[0].score
+#     def make_build_model(n_layers, best_layers, builder, units_choices):
+#         def build_model(hp):
+#             layers = best_layers.copy()
+#             new_units = hp.Choice(f"units_layers_{n_layers}", units_choices)
+#             layers.append(new_units)
+#             model_kwargs = dict(layers_list=layers)
+#             model = builder.define_model(**model_kwargs)
+#             return builder.compile(model)
+#         return build_model
 
-#             if val_loss < best_val_loss:
-#                 best_val_loss = val_loss
-#                 best_layers.append(best_hp.get(f"units_layers_{n_layers}"))
-#                 no_improve_counter = 0
-#             else:
-#                 no_improve_counter += 1
-#                 if no_improve_counter >= patience:
-#                     break
+#     for n_layers in range(1, max_layers + 1):
 
-#         return best_layers, tuners
+#         build_model = make_build_model(n_layers, best_layers, builder, units_choices)
+#         builders.append(build_model)
+
+
+# def _progressive_layer_tuning(
+#     config, builder, data,
+#     units_choices=[], max_layers=5, patience=1, max_trials=20
+# ):
+#     """ Progressively tune the number of layers and units in each layer.
+#     """
+
+#     best_layers = []
+#     best_val_loss = np.inf
+#     no_improve_counter = 0
+#     tuners = {}
+
+#     def make_build_model(n_layers, best_layers, builder, units_choices):
+#         def build_model(hp):
+#             layers = best_layers.copy()
+#             new_units = hp.Choice(f"units_layers_{n_layers}", units_choices)
+#             layers.append(new_units)
+#             model_kwargs = dict(layers_list=layers)
+#             model = builder.define_model(**model_kwargs)
+#             return builder.compile(model)
+#         return build_model
+
+#     for n_layers in range(1, max_layers + 1):
+
+#         # def build_model(hp):
+#         #     layers = best_layers.copy()
+#         #     new_units = hp.Choice(f"units_layers_{n_layers}", units_choices)
+#         #     layers.append(new_units)
+#         #     model_kwargs = dict(layers_list=layers)
+#         #     model = builder.define_model(**model_kwargs)
+#         #     return builder.compile(model)
+#         build_model = make_build_model(n_layers, best_layers, builder, units_choices)
+
+#         tuner = kt.BayesianOptimization(
+#             build_model,
+#             objective="val_loss",
+#             max_trials=max_trials,
+#             directory="progressive_tune",
+#             project_name=f"layers_{n_layers}",
+#         )
+#         tuners[n_layers] = (tuner, build_model)
+
+#         tuner.search(
+#             data.X_train,
+#             data.y_train,
+#             validation_split=config.training.validation_split,
+#             sample_weight=data.sample_weight_train,
+#             # verbose=0,
+#         )
+
+#         best_hp = tuner.get_best_hyperparameters(1)[0]
+#         val_loss = tuner.oracle.get_best_trials(1)[0].score
+
+
+#         if val_loss < best_val_loss:
+#             best_val_loss = val_loss
+#             print(best_hp)
+#             print(best_hp.values)
+
+#             best_layers.append(best_hp.get(f"units_layers_{n_layers}"))
+#             no_improve_counter = 0
+#         else:
+#             no_improve_counter += 1
+#             if no_improve_counter >= patience:
+#                 break
+
+#     return best_layers, tuners
+
+
+#     # tuner = kt.BayesianOptimization(
+#     #     build_model,
+#     #     objective="val_loss",
+#     #     max_trials=max_trials,
+#     #     directory="optimizer_tune",
+#     #     project_name="optimizer",
+#     # )
+
+#     # tuner.search(
+#     #     data.X_train,
+#     #     data.y_train,
+#     #     validation_split=config.training.validation_split,
+#     #     sample_weight=data.sample_weight_train,
+#     #     # verbose=0,
+#     # )
+
+#     # best_hp = tuner.get_best_hyperparameters(1)[0]
+#     # best_optimizer = best_hp.get("optimizer")
+#     # best_learning_rate = best_hp.get("learning_rate")
+
+#     # return (best_optimizer, best_learning_rate), tuner
+
+# # class LayerTuner(Tuner):
+# #     def __init__(self, *args, **kwargs):
+# #         super().__init__(*args, **kwargs)
+
+# #     def progressive_layer_tuning(
+# #         self, units_choices=[], max_layers=5, patience=1, max_trials=20
+# #     ):
+# #         # cfg = self.config.tuner.progressive_layer
+# #         # max_layers = cfg.max_layers
+# #         # patience = cfg.patience
+# #         # max_trials = cfg.max_trials
+# #         # new_units_choices = cfg.units_choices
+
+# #         best_layers = []
+# #         best_val_loss = np.inf
+# #         no_improve_counter = 0
+# #         tuners = {}
+
+# #         for n_layers in range(1, max_layers + 1):
+
+# #             def build_model(hp):
+# #                 layers = best_layers.copy()
+# #                 new_units = hp.Choice(f"units_layers_{n_layers}", units_choices)
+# #                 layers.append(new_units)
+# #                 model_kwargs = dict(layers_list=layers)
+# #                 model = self.builder.define_model(**model_kwargs)
+# #                 return self.builder.compile(model)
+
+# #             tuner = kt.BayesianOptimization(
+# #                 build_model,
+# #                 objective="val_loss",
+# #                 max_trials=max_trials,
+# #                 directory="progressive_tune",
+# #                 project_name=f"layers_{n_layers}",
+# #             )
+# #             tuners[n_layers] = tuner
+
+# #             tuner.search(
+# #                 self.data.X_train,
+# #                 self.data.y_train,
+# #                 validation_split=0.2,
+# #                 sample_weight=self.data.sample_weight_train,
+# #                 verbose=0,
+# #             )
+
+# #             best_hp = tuner.get_best_hyperparameters(1)[0]
+# #             val_loss = tuner.oracle.get_best_trials(1)[0].score
+
+# #             if val_loss < best_val_loss:
+# #                 best_val_loss = val_loss
+# #                 best_layers.append(best_hp.get(f"units_layers_{n_layers}"))
+# #                 no_improve_counter = 0
+# #             else:
+# #                 no_improve_counter += 1
+# #                 if no_improve_counter >= patience:
+# #                     break
+
+# #         return best_layers, tuners
